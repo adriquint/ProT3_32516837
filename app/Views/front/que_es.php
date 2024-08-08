47ee14d0411af8c5fed473a8292ad8330cc0280e
adriquint <125844291+adriquint@users.noreply.github.com>
2024-08-08 15:26:51 -0300

    Delete app directory

--- app/Views/front/que_es.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <br>
-    <h3>¿Qué es un Bonsai?</h3>
-    <br>
-</div>
--- app/Views/front/quienes_somos.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <br>
-    <h3>Nuestro equipo</h3>
-    <br>
-</div>
